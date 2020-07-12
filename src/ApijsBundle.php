3ec91f43909b05307cf4d0f0a12e3ccdea33053f
Fabrice Creuzot <code@luigifab.fr>
2020-07-12 20:00:00 +0000

    Version 6.2.0

---
+++ src/ApijsBundle.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created L/13/07/2020
+ * Updated L/13/07/2020
+ *
+ * Copyright 2008-2020 | Fabrice Creuzot (luigifab) <code~luigifab~fr>
+ * https://www.luigifab.fr/apijs
+ *
+ * This program is free software, you can redistribute it or modify
+ * it under the terms of the GNU General Public License (GPL) as published
+ * by the free software foundation, either version 2 of the license, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty, without even the implied warranty of
+ * merchantability or fitness for a particular purpose. See the
+ * GNU General Public License (GPL) for more details.
+ */
+
+namespace Luigifab\Bundle\ApijsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ApijsBundle extends Bundle {
+	public const VERSION = '6.2.0';
+}
